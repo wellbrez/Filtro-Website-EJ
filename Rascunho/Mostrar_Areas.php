eeb04f019a450ce103f3b9de44b74f0e335b2188
wellbrez <bld-wellington@hotmail.com>
2018-12-02 23:35:07 -0200

    update_bugged

--- Rascunho/Mostrar_Areas.php
+++ Rascunho/Mostrar_Areas.php
@@ -4,7 +4,31 @@ $q = $_REQUEST["q"];
 
 // process to retrieve and filter 
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "empresas_juniores";
 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,
+ $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM empresas";
+$result = mysqli_query($conn, $sql);
+$datas = array();
+$num_columns = mysqli_field_count($result);
+if ($num_columns>0)
+{
+    while ($column = mysqli_fetch_assoc($result))
+    {
+        $datas[] = $column;
+    }
+}
+mysqli_close($conn);
 
 
 
